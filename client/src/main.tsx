5ea65a5bef8fa63c66980d22909ae4da8c51f7bf
Andy-git985 <andy.yu617@gmail.com>
2024-07-24 18:12:04 -0400

    add ts config files, change main and index links

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CssBaseline, ThemeProvider, responsiveFontSizes } from '@mui/material';
-import { theme } from './styles/styles';
-import { AuthProvider } from './contexts/AuthContext';
-import { NotificationProvider } from './contexts/NotificationContext';
+import { theme } from './styles/styles.js';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import { NotificationProvider } from './contexts/NotificationContext.jsx';
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={responsiveFontSizes(theme)}>
       <CssBaseline />
